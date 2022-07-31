3b54c29f99f543c4d27a4e6c8182dd79b53ab424
Mekelaina <truemr.derpster@gmail.com>
2022-07-31 02:13:31 -0400

    added examples from previous project lexer still needs work/refactor.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,8 @@
 extern  crate substring;
+extern  crate regex; 
 
 use substring::Substring;
+use regex::{Regex, RegexSet};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -16,21 +18,20 @@ const SYMBOLS: [char; 8]  = [';', '%', '$', '#', '@', '?', '\'', '\"'];
 #[derive(Clone, Debug)]
 pub enum TokenType {
     NON,
-    MainRoutineStart,
-    MainRoutineEnd,
-    CommentMark,
-    CommentBlock,
-    LabelMark,
-    LabelName,
-    DecNumMark,
-    DecNumLiteral,
-    HexNumMark,
-    HexNumLiteral,
-    BinNumMark,
-    BinNumLiteral,
-    Keyword,
+    PercentDecimal,
+    DollarHex,
+    HashtagBinary,
+    SemicolonComment,
+    AtLabel,
+    AmpersandVariable,
+    Colon,
+    Comma,
+    QuestionArgument,
     Register,
     StringLiteral,
+    CharLiteral,
+    Number,
+    Word
 }
 
 #[derive(Debug)]
@@ -55,35 +56,53 @@ impl Token {
 
 }
 
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 pub fn lexer(filename: &str) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
+    let set = RegexSet::new(&[
+        r"[%]", //0: decimal mark
+        r"[\$]", //1: hex mark
+        r"[\#]", //2: binary mark
+        r"[;]", //3: comment mark
+        r"[@]", //4: label mark
+        r"[\&]", //5: var ref mark
+        r"[:]", //6: colon
+        r"[,]", //7: comma
+        r"[\?]", //8: arg mark
+        r"\b(s0|s1|b0|b1|w0|w1|h0|h1|f0|f1|a0|a1|x|y|c|i|o)\b", //9: registers
+        r#"""#, //10: string finder
+        r#"'"#, //11: char finder
+        r#"[0-9a-fA-F.]+"#, //12: number finder
+        r#"[a-zA-Z_0-9]+"# //13: word finder
+    ]).unwrap();
+
+    let regexes: Vec<_> = set.patterns().iter()
+    .map(|pat| Regex::new(pat).unwrap())
+    .collect();
 
     let lines = read_lines(filename).expect("Could not read file");
     for (count, line) in lines.into_iter().enumerate() {
         let l = line.unwrap();
-        let mut parts = l.trim_start().split(' ');
-        let data = l.clone();
-
-
+        let matches_loc: Vec<usize> = set.matches(&l).into_iter().collect();
+        let matches_val: Vec<&str> = set.matches(&l).into_iter()
+            .map(|match_idx| &regexes[match_idx])
+            .map(|pat| pat.find(&l).unwrap().as_str())
+            .collect();
         
-        if data.contains('"') {
-            let local = data.clone();
-            let quotes: Vec<_> = local.match_indices("\"").collect();
-            tokens.push(Token { 
-                ttype: (TokenType::StringLiteral), 
-                value: ({
-                    let v = local.as_str().substring(quotes.get(0).unwrap().0 + 1, quotes.get(1).unwrap().0).to_string();
-                    println!("{}", &v);
-                    v
-                }), 
-                line: (count + 1) })
+        for result in matches_loc.into_iter() {
+            for value in matches_val.to_owned().into_iter() {
+                println!("{}: {}", result, value);
+            }    
+        
         }
+
     }  
     tokens
         
 }
+
+
